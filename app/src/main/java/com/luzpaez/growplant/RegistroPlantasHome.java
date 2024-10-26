f237d99f96a6cacb0257eab73d95a83e8c6b5461
LuzPaez <xx.luz.esme.xx@gmail.com>
2024-10-26 18:32:57 -0500

    Corrección de error boton flotante 26_10_24

--- app/src/main/java/com/luzpaez/growplant/RegistroPlantasHome.java
+++ app/src/main/java/com/luzpaez/growplant/RegistroPlantasHome.java
@@ -1,12 +1,14 @@
 package com.luzpaez.growplant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RegistroPlantasHome extends AppCompatActivity {
 
@@ -16,5 +18,25 @@ public class RegistroPlantasHome extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_plantas_home);
 
+        ImageButton btnRegresarPrincipal = findViewById(R.id.regresar);
+        FloatingActionButton fabAddPlant = findViewById(R.id.fab_add_plant);
+
+        // Listener para el botón de regresar a la página principal
+        btnRegresarPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistroPlantasHome.this, PrincipalMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        // Listener para el botón flotante de agregar planta
+        fabAddPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistroPlantasHome.this, AgregarPlanta.class);
+                startActivity(intent);
+            }
+        });
     }
 }
