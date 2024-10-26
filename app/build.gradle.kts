b5c70af23a368f3b04b0f9afb125a85e0b4d3ecc
LuzPaez <xx.luz.esme.xx@gmail.com>
2024-10-26 18:31:27 -0500

    Agrega: dependencias voley, retrofit, json, okhttp 26_10_24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,10 +30,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
 
-dependencies {
 
+
+
+dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -42,15 +45,25 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.google.firebase:firebase-storage:21.0.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
+    // Implementación de Retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    // OkHttp (opcional, pero recomendado para una mejor gestión de las solicitudes HTTP)
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
 
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
 
-
+    implementation (libs.android.volley)// Para manejar las peticiones HTTP
+    implementation ("org.json:json:20210307") // Para manejar JSON (puedes omitir esto si ya tienes alguna librería para manejar JSON)
 
 }
